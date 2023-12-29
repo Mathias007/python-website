7e065880796c486382779573a65adca3e444750d
Mathias007 <mathias.najlepszy@gmail.com>
2023-12-29 16:42:06 +0100

    Add basic auth routes.

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,14 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('auth')
-def home():
-    return "<h1>Auth</h1>"
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
