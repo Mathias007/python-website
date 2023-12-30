275aef693ec31d2a95b1e31a465f678982915453
Mathias007 <mathias.najlepszy@gmail.com>
2023-12-30 12:33:49 +0100

    Handle user login and logout.

--- website/auth.py
+++ website/auth.py
@@ -2,16 +2,34 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -21,6 +39,8 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        user = User.query.filter_by(email=email).first()
+
         if len(email) < 4:
             flash('Email must be at least 4 characters', category='error')
         elif len(first_name) < 2:
@@ -29,6 +49,8 @@ def sign_up():
             flash("Passwords don\'t match", category="error")
         elif len(password1) < 7:
             flash("Password must be at least 7 characters", category="error")
+        elif user:
+            flash("User using this email exists in database", category="error")
         else:
             new_user = User(
                 email=email, 
@@ -37,7 +59,7 @@ def sign_up():
             )
             db.session.add(new_user)
             db.session.commit()
-
+            login_user(user, remember=True)
             flash("Account created!", category="success")
             return redirect(url_for('views.home'))
 
